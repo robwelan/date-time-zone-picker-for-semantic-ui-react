c6600ad17fe3d9ba0b3a645fbc87894fdb189e59
Rob Welan <robwelan@yahoo.com.au>
2019-06-30 16:32:32 +1000

    Move Utilities Back To Where They Should Be
     - (not in components)

--- src/components/calendar/day-controller/index.jsx
+++ src/components/calendar/day-controller/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+
 //  Semantic-UI-React
 import { Button } from 'semantic-ui-react';
 
@@ -8,7 +9,7 @@ import {
   getDayOfMonthFromDate,
   getMonthFromDate,
   getYearFromDate,
-} from '../../utilities/functions';
+} from '../../../utilities/functions';
 
 import { getDatesOfCalendarDisplay } from './functions';
 
