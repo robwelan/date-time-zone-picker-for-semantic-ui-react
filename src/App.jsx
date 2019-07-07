52acc5edcdd7cac09a56d719e7529700a11fd42d
Rob Welan <robwelan@yahoo.com.au>
2019-07-07 13:43:30 +1000

    Create A Mock API To Test With

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import DateTimeZonePicker from './date-time-zone-picker';
 // Styles
 import './App.css';
 
+let mockApi = {
+  date: {
+    year: 1975,
+    month: 6,
+    day: 20,
+  },
+  // time: {
+  //   hour: 1,
+  //   minute: 2,
+  //   second: 0,
+  //   millisecond: 0,
+  // },
+  //  zone: 'Australia/Sydney',
+};
+
 function App() {
   return (
     <div className="main">
       <div className="docs-example">
-        <DateTimeZonePicker setDate setTime setZone />
+        <DateTimeZonePicker setDate setTime setZone value={mockApi} />
       </div>
     </div>
   );
